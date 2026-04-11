58793ea198eda4e218b51b652d3e6c78d1d80159
niwyj7 <146860406+niwyj7@users.noreply.github.com>
2026-04-11 11:29:41 +0800

    Add utility functions for metrics evaluation

---
+++ src/utils.py
@@ -0,0 +1,20 @@
+def _safe_corr(a: pd.Series, b: pd.Series) -> float:
+    a = a.dropna()
+    b = b.dropna()
+    idx = a.index.intersection(b.index)
+    if len(idx) < 10:
+        return np.nan
+    return float(a.loc[idx].corr(b.loc[idx]))
+
+def _eval_metrics(y_true: pd.Series, y_pred: pd.Series) -> Dict[str, float]:
+    y_true = y_true.astype(float)
+    y_pred = y_pred.astype(float)
+    idx = y_true.index.intersection(y_pred.index)
+    y_true = y_true.loc[idx]
+    y_pred = y_pred.loc[idx]
+    mae = float(np.mean(np.abs(y_true - y_pred)))
+    rmse = float(np.sqrt(np.mean((y_true - y_pred) ** 2)))
+    mape = float(np.mean(np.abs((y_true - y_pred) / (np.maximum(np.abs(y_true), 1e-6))))) * 100.0
+    corr = _safe_corr(y_true, y_pred)
+    return {"mae": mae, "rmse": rmse, "mape_pct": mape, "corr": corr}
+
